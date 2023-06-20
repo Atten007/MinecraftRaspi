37625046baf0d6da44ea5dbfa17e254890c53a47
Atten007 <mail@mika-kreis.de>
2023-06-20 18:04:01 +0200

    Update minecraft_server.sh

--- minecraft_server.sh
+++ minecraft_server.sh
@@ -8,17 +8,73 @@ create_backup() {
     cp -r "world" "$backup_dir/backup_$timestamp"
 }
 
+# Funktion zum automatischen Aktualisieren des Servers
+update_server() {
+    echo "Möchtest du den Server automatisch aktualisieren? (j/n)"
+    read update_choice
+
+    if [[ $update_choice == "j" || $update_choice == "J" ]]; then
+        echo "Aktualisiere den Server..."
+
+        # Server stoppen
+        screen -S minecraft -X stuff 'stop'$(echo -ne '\r')
+
+        # Server-Dateien sichern
+        create_backup "$backup_dir"
+
+        # Server-Version auswählen
+        echo "Bitte gib die gewünschte Server-Version ein (z.B. 1.17.1):"
+        read server_ver
+
+        # Server JAR-Datei herunterladen
+        wget -O "$server_jar" "https://s3.amazonaws.com/Minecraft.Download/versions/${server_ver}/${server_jar}"
+        
+        echo "Der Server wurde aktualisiert."
+
+        # Server starten
+        start_server
+    else
+        echo "Der Server wird nicht aktualisiert."
+        start_server
+    fi
+}
+
+# Funktion zum Starten des Servers
+start_server() {
+    # EULA akzeptieren
+    echo "eula=true" > "$eula_file"
+
+    # Backups erstellen
+    echo "Möchtest du regelmäßige Backups erstellen? (j/n)"
+    read create_backups
+
+    if [[ $create_backups == "j" || $create_backups == "J" ]]; then
+        echo "Bitte gib den Pfad zum Verzeichnis ein, in dem die Backups gespeichert werden sollen:"
+        read backup_dir
+
+        echo "Bitte gib die gewünschte Zeit (im 24-Stunden-Format) für den Neustart des Servers ein (z.B. 03:00):"
+        read restart_time
+
+        # Cron-Job für regelmäßige Backups einrichten
+        cron_job="0 $restart_time * * * $(pwd)/create_backup.sh $backup_dir"
+        (crontab -l ; echo "$cron_job") | crontab -
+    fi
+
+    # Server starten
+    java -Xmx${ram_allocation}M -Xms${ram_allocation}M -jar "$server_jar" nogui
+}
+
 # RAM-Einstellungen
 echo "Bitte gib die RAM-Zuweisung für den Server ein (in MB):"
 read ram_allocation
 
-# Speicherort auswählen
+# Speicherort des Servers
 echo "Bitte gib den Pfad zum Verzeichnis ein, in dem die Server-Dateien gespeichert werden sollen:"
 read server_dir
 
 # Verzeichnis erstellen und in das Verzeichnis wechseln
-mkdir -p $server_dir
-cd $server_dir
+mkdir -p "$server_dir"
+cd "$server_dir"
 
 # Server-Version auswählen
 echo "Bitte gib die gewünschte Server-Version ein (z.B. 1.17.1):"
@@ -29,34 +85,4 @@ server_jar="minecraft_server.${server_ver}.jar"
 eula_file="eula.txt"
 
 # Server JAR-Datei herunterladen
-wget -O $server_jar "https://s3.amazonaws.com/Minecraft.Download/versions/${server_ver}/${server_jar}"
-
-# EULA akzeptieren
-echo "eula=true" > $eula_file
-
-# Backups erstellen
-echo "Möchtest du regelmäßige Backups erstellen? (j/n)"
-read create_backups
-
-if [[ $create_backups == "j" || $create_backups == "J" ]]; then
-    echo "Bitte gib den Pfad zum Verzeichnis ein, in dem die Backups gespeichert werden sollen:"
-    read backup_dir
-
-    echo "Bitte gib die gewünschte Zeit (im 24-Stunden-Format) für den Neustart des Servers ein (z.B. 03:00):"
-    read restart_time
-
-    # Cron-Job für regelmäßige Backups einrichten
-    cron_job="0 $restart_time * * * $(pwd)/create_backup.sh $backup_dir"
-    (crontab -l ; echo "$cron_job") | crontab -
-fi
-
-# Skript für Backuperstellung erstellen
-echo '#!/bin/bash' > create_backup.sh
-echo 'backup_dir="$1"' >> create_backup.sh
-echo 'mkdir -p "$backup_dir"' >> create_backup.sh
-echo 'timestamp=$(date +%Y%m%d%H%M%S)' >> create_backup.sh
-echo 'cp -r "world" "$backup_dir/backup_$timestamp"' >> create_backup.sh
-chmod +x create_backup.sh
-
-# Server starten
-java -Xmx${ram_allocation}M -Xms${ram_allocation}M -jar $server_jar nogui
+wget -O "$server_jar" "https://s3.amazonaws.com/Minecraft.Download
